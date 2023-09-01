1d6237c399206a509b1844e605e7ba266f0034bf
Renan.T <rtoniolo4@gmail.com>
2023-09-01 14:10:40 -0300

    small ajust

--- controllers/ListController.js
+++ controllers/ListController.js
@@ -1,15 +1,17 @@
 import { ViewList } from "../views/ViewList.js";
 import db from "../api/db/dbModel.js";
 
-const viewer = new ViewList();
-
 export class ListController {
+  constructor() {
+    this.viewer = new ViewList();
+  }
+
   async list(param1, param2) {
     const list = await db.find({ [param1]: { $in: [param2] } });
-    return viewer.uptade(list);
+    return this.viewer.uptade(list);
   }
   async listAll() {
     const list = await db.find();
-    return viewer.uptade(list);
+    return this.viewer.uptade(list);
   }
 }
