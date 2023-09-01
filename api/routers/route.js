24472dde8e9a0141c44e8ba3af653dad2be81f8d
Renan.T <rtoniolo4@gmail.com>
2023-09-01 20:28:55 -0300

    refactor

--- api/routers/route.js
+++ api/routers/route.js
@@ -1,48 +1,14 @@
 import express from "express";
-import fs from "fs";
 import { ListController } from "../../controllers/ListController.js";
 const controller = new ListController();
 
 const routers = express.Router();
 
-routers.get("/", (req, res) => {
-  res.set("Content-type", "text/html");
-  fs.readFile("./index.html", "utf-8", (err, data) => {
-    res.send(data);
-  });
-});
-routers.get("/main.js", (req, res) => {
-  res.set("Content-Type", "text/javascript");
-  fs.readFile("./main.js", "utf-8", (err, data) => {
-    res.send(data);
-  });
-});
-routers.get("/style.css", (req, res) => {
-  res.set("Content-Type", "text/css");
-  fs.readFile("./style.css", "utf-8", (err, data) => {
-    res.send(data);
-  });
-});
-routers.get(/generation-(.*)/, async (req, res) => {
-  res.set("Context-Type", "text/html");
-  const templateList = await controller.list(
-    "region",
-    req.originalUrl.substring(1)
-  );
-  res.send(templateList);
-});
-routers.get("/all", async (req, res) => {
-  res.set("Context-Type", "text/html");
-  const templateList = await controller.listAll();
-  res.send(templateList);
-});
-routers.get(/type-(.*)/, async (req, res) => {
-  res.set("Context-Type", "text/html");
-  const templateList = await controller.list(
-    "types",
-    req.originalUrl.substring(6)
-  );
-  res.send(templateList);
-});
+routers.get("/", controller.pag);
+routers.get("/main.js", controller.arqJs);
+routers.get("/style.css", controller.arqCss);
+routers.get(/generation-(.*)/, controller.generationPag);
+routers.get("/all", controller.allPag);
+routers.get(/type-(.*)/, controller.typesPag);
 
 export default routers;
--- controllers/ListController.js
+++ controllers/ListController.js
@@ -1,17 +1,49 @@
 import { ViewList } from "../views/ViewList.js";
 import db from "../api/db/dbModel.js";
+import fs from "fs";
+
+const viewer = new ViewList();
+
+async function list(param1, param2) {
+  const list = await db.find({ [param1]: { $in: [param2] } });
+  return viewer.uptade(list);
+}
+async function listAll() {
+  const list = await db.find();
+  return viewer.uptade(list);
+}
 
 export class ListController {
-  constructor() {
-    this.viewer = new ViewList();
+  async allPag(req, res) {
+    const templateList = await listAll();
+    res.send(templateList);
   }
-
-  async list(param1, param2) {
-    const list = await db.find({ [param1]: { $in: [param2] } });
-    return this.viewer.uptade(list);
+  async typesPag(req, res) {
+    res.set("Context-Type", "text/html");
+    const templateList = await list("types", req.originalUrl.substring(6));
+    res.send(templateList);
   }
-  async listAll() {
-    const list = await db.find();
-    return this.viewer.uptade(list);
+  async generationPag(req, res) {
+    res.set("Context-Type", "text/html");
+    const templateList = await list("region", req.originalUrl.substring(1));
+    res.send(templateList);
+  }
+  arqCss(req, res) {
+    res.set("Content-Type", "text/css");
+    fs.readFile("./style.css", "utf-8", (err, data) => {
+      res.send(data);
+    });
+  }
+  arqJs(req, res) {
+    res.set("Content-Type", "text/javascript");
+    fs.readFile("./main.js", "utf-8", (err, data) => {
+      res.send(data);
+    });
+  }
+  pag(req, res) {
+    res.set("Content-type", "text/html");
+    fs.readFile("./index.html", "utf-8", (err, data) => {
+      res.send(data);
+    });
   }
 }
